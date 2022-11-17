73d5330baf94ac34c8a0c60d3c8a05c068f7a5d5
Phạm Đức Đạt <91515103+Venus2602@users.noreply.github.com>
2022-11-17 20:56:15 +0700

    Add files via upload

---
+++ Demo1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Venus</title>
+</head>
+<body>
+<h1>
+    <?php 
+    $a = 6;
+    if ($a < 10){
+        echo 'Số ' . $a . ' nhỏ hơn 10';
+    } else {
+        echo 'Số ' . $a . ' lớn hơn hoặc bằng 10';
+    }
+    ?>
+</h1>
+</body>
+</html>
